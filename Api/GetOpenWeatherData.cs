45fc8c27ef024e6a13a76ea5de2efb68e02695a4
Ralf Richter <Ralf.Richter@azuredev.org>
2021-04-15 17:53:15 +0200

    Removed Key to Env

--- Api/GetOpenWeatherData.cs
+++ Api/GetOpenWeatherData.cs
@@ -6,20 +6,21 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net.Http;
+using System;
 
 namespace BlazorApp.Api
 {
     public static class GetOpenWeatherData
     {
         static HttpClient client = new HttpClient();
-
+        static string apiKey = Environment.GetEnvironmentVariable("OpenWeatherAPIKey");
         [FunctionName("GetOpenWeatherData")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             //Get Wether Data of Munich
-            var response = await client.GetAsync("https://api.openweathermap.org/data/2.5/onecall?lat=48.185777668797314&lon=11.56213033944366&exclude=minutely&appid=d5b5f7f03277369073f4155416a6a067&units=metric");
+            var response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/onecall?lat=48.185777668797314&lon=11.56213033944366&exclude=minutely&appid={apiKey}&units=metric");
             var result = await response.Content.ReadAsStringAsync();
             return new OkObjectResult(JsonConvert.DeserializeObject(result));
         }
