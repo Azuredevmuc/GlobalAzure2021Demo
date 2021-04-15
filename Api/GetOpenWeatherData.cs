2c967a2a35d13b2c43ea61188372c7ba37e6741b
Ralf Richter <Ralf.Richter@azuredev.org>
2021-04-15 18:34:02 +0200

    Fixed

--- Api/GetOpenWeatherData.cs
+++ Api/GetOpenWeatherData.cs
@@ -13,13 +13,14 @@ namespace BlazorApp.Api
     public static class GetOpenWeatherData
     {
         static HttpClient client = new HttpClient();
-        static string apiKey = Environment.GetEnvironmentVariable("OpenWeatherAPIKey");
+        
         [FunctionName("GetOpenWeatherData")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             //Get Wether Data of Munich
+            string apiKey = Environment.GetEnvironmentVariable("OpenWeatherAPIKey");
             var response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/onecall?lat=48.185777668797314&lon=11.56213033944366&exclude=minutely&appid={apiKey}&units=metric");
             var result = await response.Content.ReadAsStringAsync();
             return new OkObjectResult(JsonConvert.DeserializeObject(result));
